fb183d0fa3b48320605c5dba3da212bdeb85760a
grindlife <74398527+grindlife@users.noreply.github.com>
2021-04-19 15:49:46 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="pig",
+	version="0.1",
+	description="Test for pip install git+",
+	url="http://ss-nextpf.jp.fujitsu.com/FJH_project/gitlab/HTK.hanyu/hanyu_test.git",
+	packages=find_packages(),
+	entry_points="""
+	[console_scripts]
+	pig = pig.pig:main
+	""",
+	)
